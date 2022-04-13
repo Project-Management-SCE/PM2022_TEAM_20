8d153d1bef57f1df7b3e225f20adef3ecf1aaed7
Ariel Eps <73090605+ArielEps@users.noreply.github.com>
2022-04-13 19:47:33 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -31,8 +31,20 @@ class Views_Test(TestCase):
    
     #check Check_Password function
     def test_check_password(self):
-        self.assertEquals(False,Validation.Check_Password(111111,111111))
-        self.assertEquals(True,Validation.Check_Password(123456,1444467))
-        
+        self.assertEquals(False,views.Check_Password(111111,111111))
+        self.assertEquals(True,views.Check_Password(123456,1444467))
+
+    # check isExistsClient function
+    def test_User_is_exists(self):
+        user = User(user_name = 'Sergey',password = '123456',email='Sergey@gmail.com')
+        user.save()
+        self.assertTrue(True,views.isExistsUser('Maor','Sergey@gmail.com'))
+        self.assertFalse(False,views.isExistsUser('Vlad','Vlad@gmail.com'))
+
     
-       
+    # check Login_Exists function
+    def test_Login(self):                                                                                                
+       self.assertFalse(False,views.Login_Exists('User','Sergey','13456'))
+       self.assertTrue(True,views.Login_Exists('User','Churkin','111111'))
+       self.assertTrue(True,views.Login_Exists('Admin','Maor','123456'))
+       self.assertFalse(False,views.Login_Exists('Admin','Vlad','13456'))
