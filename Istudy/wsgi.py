0a3535b72978ad35998f513c9e249ae9f07549b5
ShaharBlank <Shahabl@ac.sce.ac.il>
2022-04-13 10:05:15 +0300

    Update and rename ProjectSettings/wsgi.py to Istudy/wsgi.py

--- Istudy/wsgi.py
+++ Istudy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectSettings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Istudy.settings')
 
 application = get_wsgi_application()
