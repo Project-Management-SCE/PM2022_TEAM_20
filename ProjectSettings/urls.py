19db6bf7217b48d0f43c42533ba222475092317c
ShaharBlank <Shahabl@ac.sce.ac.il>
2022-04-13 09:31:01 +0300

    Update urls.py

--- ProjectSettings/urls.py
+++ ProjectSettings/urls.py
@@ -8,7 +8,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('WebIStudy.urls')),
+    # path('', include('WebIStudy.urls')),
     path('accounts/', include('allauth.urls'))
 
 
