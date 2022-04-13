3b5a552b11a3be56b276db7870ea8b0f9ddc1827
ShaharBlank <Shahabl@ac.sce.ac.il>
2022-04-13 11:25:55 +0300

    Update settings.py

--- IStudy/settings.py
+++ IStudy/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'IStudy.apps.AppNameConfig'
+    'IStudy'
     # 'WebIStudy.apps.WebistudyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
