23a317bf2a9e9a70c9a22050072b076d950e3ffc
ShaharBlank <Shahabl@ac.sce.ac.il>
2022-04-13 09:26:41 +0300

    Update wsgi.py

--- ProjectSettings/wsgi.py
+++ ProjectSettings/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IStudy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectSettings.settings')
 
 application = get_wsgi_application()
