7e0dd68ee13306dcc12694e512ad81f23b7cc327
Ariel Eps <73090605+ArielEps@users.noreply.github.com>
2022-04-26 18:10:40 +0300

    Delete Frontend directory

--- Frontend/JavaScript/ForumSelect.js
+++
@@ -1,9 +0,0 @@
-function Menu()
-{
-    var menu = document.getElementById("Profile_Select");
-
-    if(menu.style.display == "flex")
-        menu.style.display="none";
-    else
-        menu.style.display="flex";
-}
