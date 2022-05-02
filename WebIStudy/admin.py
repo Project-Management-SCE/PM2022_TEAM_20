86008aecb7b788bc9ff7015dc9c02f859cf90122
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-02 20:12:58 +0300

    BSPM2022T20-(60-64) & BSPM2022T20-(113-117) & BSPM2022T20-(52-54)

--- WebIStudy/admin.py
+++ WebIStudy/admin.py
@@ -4,4 +4,6 @@ from .models import *
 admin.site.register(User)
 admin.site.register(Admin)
 admin.site.register(Forum)
+admin.site.register(ForumMessage)
+admin.site.register(Comments)
 # Register your models here.
