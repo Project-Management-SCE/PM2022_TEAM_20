e05a07165a5ba590174b3f72fab23fbeca6e9b7b
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-02 20:14:16 +0300

    BSPM2022T20-(60-64) & BSPM2022T20-(113-117) & BSPM2022T20-(52-54)

--- WebIStudy/urls.py
+++ WebIStudy/urls.py
@@ -3,6 +3,8 @@ from WebIStudy.views import *
 from django.urls import include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+
 app_name = 'WebIStudy'
 
 urlpatterns = [
@@ -27,8 +29,23 @@ urlpatterns = [
     re_path(r'AddPasswordPage/(?P<oid>[-\w]*)',ShowAddPasswordPage, name='AddPassword'),  
     re_path(r'PasswordAdded/(?P<oid>[-\w]*)',AddPasswordToForum, name='passwordAdded'), 
     re_path(r'ForumPage/(?P<oid>[-\w]*)',MoveToForumManagePage, name='ManageForum'), 
+    re_path(r'UserSelectedForum/(?P<oid>[-\w]*)/(?P<foru>[-\w]*)',ShowUserForum, name='UserForum'), 
+    re_path(r'UserPostAMessage/(?P<oid>[-\w]*)/(?P<foru>[-\w]*)',UserPostAmessage, name='Usermessage'),
+    re_path(r'MessagesPageShow/(?P<oid>[-\w]*)/(?P<Author>[-\w]*)/(?P<Subject>[-\w]*)',UserMessageShowMessages, name='MessagesPage'),  
+    re_path(r'MessagesAfterComments/(?P<oid>[-\w]*)/(?P<Author>[-\w]*)/(?P<Subject>[-\w]*)',AddCommentForMessage, name='MessagesCommentsPage'),  
+
+
+    re_path(r'UserDeleteAMessage/(?P<oid>[-\w]*)/(?P<foru>[-\w]*)',UserPostAmessageDelete, name='UsermessageDelete'),
+    re_path(r'CommanntPagesUserAdminPage/(?P<oid>[-\w]*)/(?P<Author>[-\w]*)/(?P<Subject>[-\w]*)',ManageUserMessageShowMessages, name='ManageMessagesPage'),  
+    re_path(r'Messagedeletepagemanydeletemessages/(?P<oid>[-\w]*)/(?P<Author>[-\w]*)/(?P<Subject>[-\w]*)',deleteMessageManyMessage, name='DeleteMessageManyMessage'), 
+
+    re_path(r'search/(?P<oid>[-\w]*)/',Search, name='searchuser'), 
+
+
+    path('accounts/profile/',HomePage, name='testing'),
+
+    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-
