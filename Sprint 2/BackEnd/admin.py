fdd8d7c476aab0edafa6346c40e0d54230df8fba
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-02 19:34:31 +0300

    Delete Sprint 2 directory

--- Sprint 2/BackEnd/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(User)
-admin.site.register(Admin)
-admin.site.register(Forum)
-admin.site.register(ForumMessage)
-admin.site.register(Comments)
-# Register your models here.
