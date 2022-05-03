bbd93a169f655f2f22d81f8a953a44b13f77ba96
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-03 12:54:47 +0300

    Backend Sprint 1-2

--- WebIStudy/views.py
+++ WebIStudy/views.py
@@ -202,19 +202,177 @@ def UserManageAction(request, oid):
 
     return render(request, "HTML/ForumAdmin.html", {'user':user,'forum':forum})  
 
+def ShowAddPasswordPage(request, oid):
+    admin = Admin.objects.get(user_name = oid)
+    forum = Forum.objects.all()   
+    
+    return render(request, "HTML/AddForumPassword.html", {'admin':admin,'forum':forum})  
+
+def AddPasswordToForum(request, oid):
+    user = Admin.objects.get(user_name = oid)
+
+    passwo = request.POST.getlist('pass')
+
+    Forums_Names = request.POST.getlist('item')
+    Passwords = []
+
+    for i in passwo:
+        if i != '':
+            Passwords.append(i)
+
+    for i in list(range(len(Forums_Names))):
+        name = Forums_Names[i]
+        passw = Passwords[i]
+        f = Forum.objects.get(Forum_name = name)
+        f.password = passw
+        f.save()
 
 
+    forum = Forum.objects.all()  
+    return render(request, "HTML/ForumAdmin.html", {'user':user,'forum':forum})  
+
+def MoveToForumManagePage(request, oid):
+
+    Pass = request.POST['password']
+    ForumName = request.POST['select']
+
+    user = User.objects.get(user_name = oid)
+    forum = Forum.objects.get(Forum_name = ForumName)
+    forumMessage = ForumMessage.objects.filter(Forum_name = ForumName)
+    
+    if forum.password == Pass and user.forum_manage == ForumName:
+        Pictures = User.objects.all()
+        return render(request, "HTML/ManagerForumPage.html", {'user':user,'forum':forum, 'forumMessage':forumMessage, 'Pictures':Pictures})  
+
+    return HttpResponse("Bad!!")
+
+def ShowUserForum(request, oid, foru):
+    user = User.objects.get(user_name = oid)
+    
+    forum = Forum.objects.all()
+
+    for i in forum:
+        if foru in list(i.Forum_name.split()):
+            foru = i.Forum_name
 
 
+    forum = Forum.objects.get(Forum_name = foru)
+    forumMessage = ForumMessage.objects.filter(Forum_name = foru)
+
+    Pictures = User.objects.all()
+
+    return render(request, "HTML/UserForumPage.html", {'user':user,'forum':forum, 'forumMessage':forumMessage , 'Pictures':Pictures})  
 
 
+def UserPostAmessage(request, oid, foru):
+    user = User.objects.get(user_name = oid)
+
+    Sub = request.POST['name1']
+    Mess = request.POST['name2']
 
 
+    forum = Forum.objects.all()
+
+    for i in forum:
+        if foru in list(i.Forum_name.split()):
+            foru = i.Forum_name
+    forum = Forum.objects.get(Forum_name = foru)
+
+    message= ForumMessage(Forum_name = foru,Author = oid ,subject=Sub, message=Mess )
+    message.save()
+
+    forumMessage = ForumMessage.objects.filter(Forum_name = foru)
+    Pictures = User.objects.all()
+    return render(request, "HTML/UserForumPage.html", {'user':user,'forum':forum, 'forumMessage':forumMessage, 'Pictures':Pictures})  
+
+def UserMessageShowMessages(request, oid, Author, Subject):
+    user = User.objects.get(user_name = oid)
+
+    forumMessage = ForumMessage.objects.get(Author = Author, subject = Subject)
+
+    comments = Comments.objects.filter(sender = Author, subject = Subject)
+    Pictures = User.objects.all()
+    return render(request, "HTML/UserMessagePage.html", {'user':user,'comments':comments, 'forumMessage':forumMessage, 'Pictures':Pictures})  
 
 
+def AddCommentForMessage(request, oid, Author, Subject ):
+    Mess = request.POST['name']
+    
+    user = User.objects.get(user_name = oid)
+
+    forumMessage = ForumMessage.objects.get(Author = Author, subject = Subject)
+
+    comm= Comments(sender = Author,subject = Subject ,Author=oid, message=Mess )
+    comm.save()
+
+    comments = Comments.objects.filter(sender = Author, subject = Subject)
+    Pictures = User.objects.all()
+    return render(request, "HTML/UserMessagePage.html", {'user':user,'comments':comments, 'forumMessage':forumMessage,  'Pictures':Pictures})  
 
 
+def UserPostAmessageDelete(request, oid, foru):
+
+    user = User.objects.get(user_name = oid)
+
+    test = request.POST.getlist('item')
+
+    forum = Forum.objects.all()
+
+    for i in forum:
+        if foru in list(i.Forum_name.split()):
+            foru = i.Forum_name
+    forum = Forum.objects.get(Forum_name = foru)
+
+    for i in test:
+        j = i.split()
+        for k in Comments.objects.filter(sender = j[1], subject = j[0]):
+            k.delete()
+        a = ForumMessage.objects.get(subject = j[0] ,Author= j[1])
+        a.delete()
+
+    forumMessage = ForumMessage.objects.filter(Forum_name = foru)
+
+    Pictures = User.objects.all()
+    return render(request, "HTML/ManagerForumPage.html", {'user':user,'forum':forum, 'forumMessage':forumMessage , 'Pictures':Pictures})  
+
+def ManageUserMessageShowMessages(request, oid, Author, Subject):
+    user = User.objects.get(user_name = oid)
+
+    forumMessage = ForumMessage.objects.get(Author = Author, subject = Subject)
+
+    comments = Comments.objects.filter(sender = Author, subject = Subject)
+    Pictures = User.objects.all()
+    return render(request, "HTML/ManagerMessagePage.html", {'user':user,'comments':comments, 'forumMessage':forumMessage, 'Pictures':Pictures})  
+
+def deleteMessageManyMessage(request, oid, Author, Subject ):
+    user = User.objects.get(user_name = oid)
+
+    test = request.POST.getlist('item')
+
+    for i in test:
+        j = i.split('papa1')
+        k = Comments.objects.get(sender = j[1], subject = j[0], Author=j[2] ,message = j[3])
+        k.delete()
 
 
+    forumMessage = ForumMessage.objects.get(Author = Author, subject = Subject)
+
+    comments = Comments.objects.filter(sender = Author, subject = Subject)
+    Pictures = User.objects.all()
+    return render(request, "HTML/ManagerMessagePage.html", {'user':user,'comments':comments, 'forumMessage':forumMessage,  'Pictures':Pictures})  
 
 
+def Search(request, oid):
+    username = request.POST['searchbar']
+
+    if isExistsUser(username):
+        admin = Admin.objects.get(user_name = oid)
+        user = User.objects.filter(user_name = username)
+        forum = Forum.objects.all()   
+        return render(request, "HTML/UserManagement.html", {'admin':admin, 'user':user, 'forum':forum})  
+
+    admin = Admin.objects.get(user_name = oid)
+    user = User.objects.all()
+    forum = Forum.objects.all()   
+    
+    return render(request, "HTML/UserManagement.html", {'admin':admin, 'user':user, 'forum':forum})  
