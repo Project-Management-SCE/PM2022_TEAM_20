e44d94adacbc449330fa875c701b8166224ecc24
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-03 13:02:46 +0300

    Sprint 1-2

--- IStudy/urls.py
+++ IStudy/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import include, re_path
 from django.urls import path , include
 
+from django.urls import path
+from WebIStudy.views import *
+from django.urls import include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 #from .models import Manager,Employee,Client
 
@@ -13,3 +18,5 @@ urlpatterns = [
 
 
 ]
+
+
