df53ed82a6c49d462666501d930650b9e56ce22c
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-03 19:18:52 +0300

    Sprint 1 & Sprint 2

--- WebIStudy/tests.py
+++ WebIStudy/tests.py
@@ -45,3 +45,16 @@ class Views_Test(TestCase):
        self.assertTrue(True,views.Login_Exists('User','Churkin','111111'))
        self.assertTrue(True,views.Login_Exists('Admin','Maor','123456'))
        self.assertFalse(False,views.Login_Exists('Admin','Vlad','13456'))
+
+
+
+    def test_CheckForumExist(self):
+       self.assertFalse(False,views.CheckForumExist('Computer Arcithecture'))
+       self.assertTrue(True,views.CheckForumExist('Computer Science'))
+       self.assertTrue(True,views.CheckForumExist('Electrical Engineering'))
+       self.assertTrue(True,views.CheckForumExist('Industrial Engineering and Management'))
+
+    def test_Check_if_Forum_Manager(self):
+        self.assertFalse(False,views.Check_if_Forum_Manager('Moshiko', 'Computer Science', '111211')) 
+        self.assertTrue(True,views.Check_if_Forum_Manager('Moshiko', 'Computer Science', '111111')) 
+        self.assertFalse(False,views.Check_if_Forum_Manager('Sergey', 'Electrical Engineering', '123441')) 
