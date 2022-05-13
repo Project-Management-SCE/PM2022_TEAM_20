590faeee7dbcc50b36c713ae01d3b5a30fdc5b9b
Ariel Eps <73090605+ArielEps@users.noreply.github.com>
2022-05-13 12:15:03 +0300

    Update settings.py

--- IStudy/settings.py
+++ IStudy/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-5*f3o0yye)%@o4lw9%xvo2xlem%j2swtg-5^&!!o$@-e&d1nr3
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['webistudy.herokuapp.com']
 
