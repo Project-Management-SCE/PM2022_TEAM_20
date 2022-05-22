9f9704f48f786038926bfecc96fec1cfcb2a4e53
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-22 12:08:25 +0300

    BSPM2022T20-128 BSPM2022T20-129 BSPM2022T20-163

--- WebIStudy/static/JAVASCRIPT/UserMessagePage.js
+++ WebIStudy/static/JAVASCRIPT/UserMessagePage.js
@@ -1,4 +1,3 @@
-//Functionallity
 var flag=0;
 document.querySelector(".user_botton").addEventListener("click",function(){
   if(flag == 0){
@@ -18,4 +17,14 @@ document.querySelector("#addButton").addEventListener("click",function(){
 document.querySelector("#post").addEventListener("click",function(){
   document.querySelector(".newMessage").classList.add("display");
   document.querySelector("#addButton").classList.remove("display");
+})
+
+let buttonsArray=document.querySelectorAll(".contact_Button");
+buttonsArray.forEach(function(eleme){
+  eleme.addEventListener("click",function(){
+    sibling=this.previousElementSibling.previousElementSibling.previousElementSibling;
+    address=sibling.textContent;
+    document.querySelector("#sendMail").setAttribute('action',"mailto:"+address);
+    document.querySelector(".compose").click();
+  })
 })
