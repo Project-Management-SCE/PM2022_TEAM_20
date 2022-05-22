55096f96a219ee74e13237ed408b7427da6fc473
MaorMohav <73097083+MaorMohav@users.noreply.github.com>
2022-05-22 12:02:14 +0300

    BSPM2022T20-69  BSPM2022T20-153  BSPM2022T20-154

--- WebIStudy/static/JAVASCRIPT/UserManagement.js
+++ WebIStudy/static/JAVASCRIPT/UserManagement.js
@@ -1,13 +1,14 @@
 var sibling;
 var address;
-let elementsArray = document.querySelectorAll(".check");
+/*let elementsArray = document.querySelectorAll(".check");
 elementsArray.forEach(function(elem) {
     elem.addEventListener("click", function() {
-      sibling=this.parentElement.nextElementSibling.nextElementSibling;
+      sibling=this.nextElementSibling.nextElementSibling.nextElementSibling;
       address=sibling.textContent;
       document.querySelector("#sendMail").setAttribute('action',"mailto:"+address);
     });
 });
+*/
 var flag=0;
 document.querySelector(".user_botton").addEventListener("click",function(){
   if(flag == 0){
@@ -18,4 +19,14 @@ document.querySelector(".user_botton").addEventListener("click",function(){
     document.querySelector("#Profile_Select").classList.add("invisible");
     flag=0;
   }
+})
+
+let buttonsArray=document.querySelectorAll(".contact_Button");
+buttonsArray.forEach(function(eleme){
+  eleme.addEventListener("click",function(){
+    sibling=this.parentElement.previousElementSibling.previousElementSibling.previousElementSibling;
+    address=sibling.textContent;
+    document.querySelector("#sendMail").setAttribute('action',"mailto:"+address);
+    document.querySelector(".compose").click();
+  })
 })
